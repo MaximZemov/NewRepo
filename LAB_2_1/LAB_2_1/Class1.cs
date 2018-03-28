7952cc613559bef61226638ed1b81aadeb1238b8
Maxim <zm62rus@mail.ru>
2018-03-28 22:04:34 +0300

    Версия 1

---
+++ LAB_2_1/LAB_2_1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2_1
+{
+    public class Class1
+    {
+        public string q;
+        public double a, b;
+
+        public void Metod()
+        {
+           if (q == "1")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат умножения = {0}", a * b);
+            }
+            if (q == "2")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат деления = {0}", a / b);
+            }
+            if (q == "3")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат сложения = {0}", a + b);
+            }
+            if (q == "4")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат вычитания = {0}", a - b);
+            }
+            if (q == "5")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Результат возведения числа в степень = {0}", Math.Pow(a, b));
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- LAB_2_1/LAB_2_1/Program.cs
+++ LAB_2_1/LAB_2_1/Program.cs
@@ -8,8 +8,11 @@ namespace LAB_2_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Консольный калькулятор (бета-версия 1.0)");
+            Console.WriteLine(' ');
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(@"Выберите арифметическое действие:
             - Умножение (введите 1)
@@ -17,48 +20,19 @@ namespace LAB_2_1
             - Сложение (введите 3)
             - Вычитание (введите 4)
             - Возведение числа в степень (введите 5)");
+            Class1 c = new Class1();
             Console.ForegroundColor = ConsoleColor.Red;
-            string q = Console.ReadLine();
-            double a, b;
-            Console.WriteLine(' ');
+            c.q = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Введите первое значение");
             Console.ForegroundColor = ConsoleColor.Red;
-            a = double.Parse(Console.ReadLine());
+            c.a = double.Parse(Console.ReadLine());
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Введите второе значение");
             Console.ForegroundColor = ConsoleColor.Red;
-            b = double.Parse(Console.ReadLine());
+            c.b = double.Parse(Console.ReadLine());
             Console.WriteLine(' ');
-
-            static 
-                { }
-                if (q == "1")
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Результат умножения = {0}", a * b);
-            }
-            if (q == "2")
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Результат деления = {0}", a / b);
-            }
-            if (q == "3")
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Результат сложения = {0}", a + b);
-            }
-            if (q == "4")
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Результат вычитания = {0}", a - b);
-            }
-            if (q == "5")
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Результат возведения числа в степень = {0}", Math.Pow(a, b));
-            }
-            Console.ReadKey();
+            c.Metod();
         }
     }
 }
