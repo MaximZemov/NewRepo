bba1fe73ea2525624e33d0defa0a2a035d87404e
Maxim <zm62rus@mail.ru>
2018-03-29 20:12:54 +0300

    Массивы

---
+++ LAB_2_2/LAB_2_2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2_2
+{
+    public class Class1
+    {
+        public int n;
+        public int t;
+
+       
+        public void Massiv1()
+        {
+            int[,] arr1 = new int[n, t];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write("Введите arr1[{" + i + "},{" + j + "}] : ");
+                    arr1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine(" ");
+        }
+
+        public void Massiv2()
+        {
+            int[,] arr2 = new int [n,t];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < t; j++)
+                {
+                    Console.Write("Введите arr[{" + i + "},{" + j + "}] : ");
+                    arr2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ LAB_2_2/LAB_2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность 2-х массивов ");
+            Class1 c = new Class1();
+            c.n = int.Parse(Console.ReadLine());
+            c.t = int.Parse(Console.ReadLine());
+            c.Massiv1();
+            c.Massiv2();
+        }
+    }
+}
