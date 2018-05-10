49b419cab0bc6cf50c7503e642fd8fef64830f9c
Maxim <zm62rus@mail.ru>
2018-05-10 21:24:41 +0300

    NUnit_LAB6

---
+++ NUnit.Tests1/NUnit.Tests1/Proverki.cs
@@ -0,0 +1,164 @@
+﻿using GeometricFigures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class Proverki
+    {
+        [Test]
+        public void TriangleAreaFull()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                for (int j = 0; j <= 20; j++)
+                {
+                    for (int k = 0; k <= 20; k++)
+                    {
+                        try
+                        {
+                            Triangle triangle = new Triangle(i, j, k);
+                            double per = 1 / 2 * (i + j + k);
+                            double area = Math.Sqrt(per * (per - i) * (per - j) * (per - k));
+                            Assert.AreEqual(Math.Round(area, 2), Math.Round(triangle.getAreaTriangle(), 2));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Ошибка данных ( {0} ,{1}, {2} ) ", i, j, k);
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        [Test]
+        public void TriangleLenghtFull()
+        {
+
+            for (int i = 0; i <= 20; i++)
+            {
+                for (int j = 0; j <= 20; j++)
+                {
+                    for (int k = 0; k <= 20; k++)
+                    {
+                        try
+                        {
+                            Triangle triangle = new Triangle(i, j, k);
+                            double lenght = i + j + k;
+                            Assert.AreEqual(Math.Round(lenght, 2), Math.Round(triangle.getLengthTriangle(), 2));
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Ошибка данных ( {0} ,{1}, {2} ) ", i, j, k);
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+
+        [Test]
+        public void CircleLetter()
+        {
+            Exception exception = null;
+            try
+            {
+                Circle circle = new Circle(Convert.ToInt32("incorrect type"));
+            }
+            catch (Exception excep)
+            {
+                exception = excep;
+            }
+            Assert.IsNull(exception);
+        }
+
+
+        [Test]
+        public void CircleWithMines()
+        {
+            Exception exception = null;
+            try
+            {
+                Circle circle = new Circle(-4);
+            }
+            catch (Exception excep)
+            {
+                exception = excep;
+            }
+            Assert.IsNotNull(exception);
+        }
+
+        [Test]
+        public void SquareLetter()
+        {
+            Exception exception = null;
+            try
+            {
+                Square circle = new Square(Convert.ToInt32("incorrect type"));
+            }
+            catch (Exception excep)
+            {
+                exception = excep;
+            }
+            Assert.IsNull(exception);
+        }
+
+
+        [Test]
+        public void SquareWithMines()
+        {
+            Exception exception = null;
+            try
+            {
+                Square circle = new Square(-4);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+        }
+        [Test]
+        public void TriangleLetter()
+        {
+            Exception exception = null;
+            try
+            {
+                Triangle circle = new Triangle(Convert.ToInt32("incorrect type"), Convert.ToInt32("incorrect type"), Convert.ToInt32("incorrect type"));
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNull(exception);
+        }
+
+
+        [Test]
+        public void TriangleWithMines()
+        {
+            Exception exception = null;
+            try
+            {
+                Triangle circle = new Triangle(-2, -2, -2);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Assert.IsNotNull(exception);
+        }
+    }
+}
---
+++ NUnit.Tests1/NUnit.Tests1/Tests_po_zacheniam.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using GeometricFigures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class Tests_po_zacheniam
+    {
+        double pi = 3.141593;
+
+        public static object[] GeometricCircleSquare =
+       {
+                new object[] { 0 },
+                new object[] { 1 },
+                new object[] { 2 },
+                new object[] { 3 },
+                new object[] { 4 },
+                new object[] { 5 },
+                new object[] { 6 },
+                new object[] { 7 },
+                new object[] { 8 },
+                new object[] { 9 },
+                new object[] { 10 },
+                new object[] { 11 },
+                new object[] { 12 },
+                new object[] { 13 },
+                new object[] { 14 },
+                new object[] { 15 },
+                new object[] { 16 },
+                new object[] { 17 },
+                new object[] { 18 },
+                new object[] { 19 },
+                new object[] { 20 }
+
+        };
+        public static object[] GeometricTriangle =
+        {
+                new object[] { 0, 0, 0 },
+                new object[] { 0, 1, 1 },
+                new object[] { 1, 1, 0 },
+                new object[] { 1, 0, 1 },
+                new object[] { 1, 1, 1 },
+                new object[] { 7, 9, 12 },
+                new object[] { 12, 16, 3 },
+                new object[] { 5, 12, 12 },
+                new object[] { 19, 16, 13 },
+                new object[] { 4, 14, 9 },
+                new object[] { 10, 10, 10 },
+                new object[] { 20, 20, 20 }
+        };
+
+        [Test, TestCaseSource("GeometricCircleSquare")]
+        public void CircleArea(int n)
+        {
+            Circle circle = new Circle(n);
+            double area = pi * n * n;
+            Assert.AreEqual(Math.Round(area, 1), Math.Round(circle.getAreaCircle(), 1));
+        }
+
+        [Test, TestCaseSource("GeometricCircleSquare")]
+        public void CircleLenght(int n)
+        {
+            Circle circle = new Circle(n);
+            double lenght = 2 * pi * n;
+            Assert.AreEqual(Math.Round(lenght, 1), Math.Round(circle.getLengthCircle(), 1));
+        }
+        
+        [Test, TestCaseSource("GeometricCircleSquare")]
+        public void SquareArea(int n)
+        {
+            Square square = new Square(n);
+            double area = n * n;
+            Assert.AreEqual(Math.Round(area, 1), Math.Round(square.getAreaSquare(), 1));
+        }
+
+        [Test, TestCaseSource("GeometricCircleSquare")]
+        public void SquareLenght(int n)
+        {
+            Square square = new Square(n);
+            double lenght = 4 * n;
+            Assert.AreEqual(Math.Round(lenght, 2), Math.Round(square.getLengthSquare(), 2));
+        }
+        
+        [Test, TestCaseSource("GeometricTriangle")]
+        public void TriangleLenght(int n, int p, int q)
+        {
+            Triangle triangle = new Triangle(n, p, q);
+            double lenght = n + p + q;
+            Assert.AreEqual(Math.Round(lenght, 1), Math.Round(triangle.getLengthTriangle(), 1));
+        }
+
+        [Test, TestCaseSource("GeometricTriangle")]
+        public void TriangleArea(int n, int p, int q)
+        {
+            Triangle triangle = new Triangle(n, p, q);
+            double per =(n + p + q);
+            double area = Math.Sqrt(per * (per - n) * (per - p) * (per - q));
+            Assert.AreEqual(Math.Round(area, 1), Math.Round(triangle.getAreaTriangle(), 1));
+        }
+        
+    }
+}
+
